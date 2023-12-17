022be1de1e2b1e9342f7447c3171781a0060b960
Timur <152402728+timtimer11@users.noreply.github.com>
2023-12-17 10:30:41 +0800

    Update main.py

--- Cloud Function 2/main.py
+++ Cloud Function 2/main.py
@@ -11,31 +11,23 @@ client = bigquery.Client(credentials=credentials)
 
 current_date = datetime.datetime.now().strftime("%Y%m%d")
 table_id = f"stockswhatsup.stocks_data.{current_date}"
+def load_to_bq(event, context):
+  dataframe = get_dataframe()
 
-dataframe = get_dataframe()
+  job_config = bigquery.LoadJobConfig(
+      schema=[
+          bigquery.SchemaField("stock_symbol", bigquery.enums.SqlTypeNames.STRING),
+          bigquery.SchemaField("trading_volume", bigquery.enums.SqlTypeNames.FLOAT),
+          bigquery.SchemaField("volume_weighted_avg_price", bigquery.enums.SqlTypeNames.FLOAT),
+          bigquery.SchemaField("open_price", bigquery.enums.SqlTypeNames.FLOAT),
+          bigquery.SchemaField("close_price", bigquery.enums.SqlTypeNames.FLOAT),
+          bigquery.SchemaField("highest_price", bigquery.enums.SqlTypeNames.FLOAT),
+          bigquery.SchemaField("lowest_price", bigquery.enums.SqlTypeNames.FLOAT),
+          bigquery.SchemaField("unix_timestamp", bigquery.enums.SqlTypeNames.INT64),
+          bigquery.SchemaField("number_of_transactions", bigquery.enums.SqlTypeNames.INT64),
+      ]
+  )
 
-job_config = bigquery.LoadJobConfig(
-    schema=[
-        bigquery.SchemaField("stock_symbol", bigquery.enums.SqlTypeNames.STRING),
-        bigquery.SchemaField("trading_volume", bigquery.enums.SqlTypeNames.FLOAT),
-        bigquery.SchemaField("volume_weighted_avg_price", bigquery.enums.SqlTypeNames.FLOAT),
-        bigquery.SchemaField("open_price", bigquery.enums.SqlTypeNames.FLOAT),
-        bigquery.SchemaField("close_price", bigquery.enums.SqlTypeNames.FLOAT),
-        bigquery.SchemaField("highest_price", bigquery.enums.SqlTypeNames.FLOAT),
-        bigquery.SchemaField("lowest_price", bigquery.enums.SqlTypeNames.FLOAT),
-        bigquery.SchemaField("unix_timestamp", bigquery.enums.SqlTypeNames.INT64),
-        bigquery.SchemaField("number_of_transactions", bigquery.enums.SqlTypeNames.INT64),
-    ]
-)
-
-job = client.load_table_from_dataframe(
-    dataframe, table_id, job_config=job_config
-)
-job.result()
-
-table = client.get_table(table_id)
-print(
-    "Loaded {} rows and {} columns to {}".format(
-        table.num_rows, len(table.schema), table_id
-    )
-)
+  job = client.load_table_from_dataframe(
+      dataframe, table_id, job_config=job_config
+  )
